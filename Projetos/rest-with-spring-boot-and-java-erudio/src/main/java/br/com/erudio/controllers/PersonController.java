3fcba55ca29ea81a91ef3646cc823521577f5f29
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-02-01 00:37:12 -0300

    Aula - Substituindo os Mocks do Service pelo Acesso ao Repositório

--- Projetos/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ Projetos/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -27,7 +27,7 @@ public class PersonController {
     }	
 	
     @RequestMapping(value = "/{id}", method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable("id") String  id) throws Exception {
+    public Person findById(@PathVariable("id") Long id) throws Exception {
         return service.findById(id);
     }
     
@@ -46,7 +46,7 @@ public class PersonController {
     }      
     
     @RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
-    public void delete(@PathVariable("id") String  id) throws Exception {
+    public void delete(@PathVariable("id") Long id) throws Exception {
         service.delete(id);
     }    
 }
--- Secao05/14 - Substituindo os Mocks do Service pelo Acesso ao Repositório/PersonServices.java
+++ Secao05/14 - Substituindo os Mocks do Service pelo Acesso ao Repositório/PersonServices.java
@@ -17,41 +17,50 @@ public class PersonServices {
 	
 	@Autowired
 	PersonRepository repository;
-	
+
 	public List<Person> findAll() {
+
 		logger.info("Finding all people!");
-	
+
 		return repository.findAll();
 	}
-	
+
 	public Person findById(Long id) {
 		
 		logger.info("Finding one person!");
 		
-		return repository.findById(id).orElseThrow( () -> new ResourceNotFoundException("No records found this ID!") );
+		return repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 	}
 	
 	public Person create(Person person) {
-		logger.info("Create one person!");
+
+		logger.info("Creating one person!");
+		
 		return repository.save(person);
 	}
 	
 	public Person update(Person person) {
-		logger.info("Update one person!");
-		var entity = repository.findById(person.getId()).orElseThrow( () -> new ResourceNotFoundException("No records found this ID!") );
 		
+		logger.info("Updating one person!");
+		
+		var entity = repository.findById(person.getId())
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
 		entity.setFirstName(person.getFirstName());
 		entity.setLastName(person.getLastName());
 		entity.setAddress(person.getAddress());
-		entity.setGender(person.getGender());		
+		entity.setGender(person.getGender());
 		
 		return repository.save(person);
-	}	
+	}
 	
 	public void delete(Long id) {
-		logger.info("Delete one person!");
-
-		var entity = repository.findById(id).orElseThrow( () -> new ResourceNotFoundException("No records found this ID!") );
+		
+		logger.info("Deleting one person!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		repository.delete(entity);
-	}	
+	}
 }
