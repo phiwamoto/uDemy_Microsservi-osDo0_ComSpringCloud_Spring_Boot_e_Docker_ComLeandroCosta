c2d0cde4ed1b3aae305f1a25a63a226aeea32a46
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-01-31 23:48:32 -0300

    Aula - Configurando conexao com o banco de dados MySQL + aulas anteriores anteriores

---
+++ Secao05/07 - Definindo a Lógica da Nossa Aplicação/MathController.java
@@ -0,0 +1,27 @@
+package br.com.erudio;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+    
+    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+    }    
+    
+    public static Double covertToDouble(String strNumber) {
+        return 1.0d;
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        return false;
+    }
+}
