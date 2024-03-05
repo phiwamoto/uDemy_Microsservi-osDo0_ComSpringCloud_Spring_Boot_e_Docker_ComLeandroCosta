50313f9f23f40b552dc59cac489c2ba9e0942c37
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-03-04 23:48:59 -0300

    Aula - Recuperando a Porta da Instância em Execução

--- Projetos/cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
+++ Projetos/cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.erudio.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,9 @@ import br.com.erudio.model.Cambio;
 @RequestMapping("cambio-service")
 public class CambioController {
 	
+	@Autowired
+	private Environment environment;
+	
 	// http://localhost:8000/cambio-service/5/USD/BRL
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
@@ -20,7 +25,9 @@ public class CambioController {
 			@PathVariable("from") String from,
 			@PathVariable("to") String to
 			) {
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+		var port = environment.getProperty("local.server.port");
+		
+		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 	
 }
