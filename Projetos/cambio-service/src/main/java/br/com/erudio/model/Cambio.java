da28d6e86a9b7036d6f00665be925d88a0685d32
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-03-04 23:40:41 -0300

    Aula - Retornando Mocks e Testando o Cambio Service

---
+++ Projetos/cambio-service/src/main/java/br/com/erudio/model/Cambio.java
@@ -0,0 +1,98 @@
+package br.com.erudio.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Cambio implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionFactor;
+	private BigDecimal conversionValue;
+	private String environment;
+
+	public Cambio() {
+
+	}	
+	
+	public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal conversionValue, String environment) {
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.conversionValue = conversionValue;
+		this.environment = environment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+
+	public void setConversionFactor(BigDecimal conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	public BigDecimal getConversionValue() {
+		return conversionValue;
+	}
+
+	public void setConversionValue(BigDecimal conversionValue) {
+		this.conversionValue = conversionValue;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conversionFactor, conversionValue, environment, from, id, to);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cambio other = (Cambio) obj;
+		return Objects.equals(conversionFactor, other.conversionFactor)
+				&& Objects.equals(conversionValue, other.conversionValue)
+				&& Objects.equals(environment, other.environment) && Objects.equals(from, other.from)
+				&& Objects.equals(id, other.id) && Objects.equals(to, other.to);
+	}
+}
