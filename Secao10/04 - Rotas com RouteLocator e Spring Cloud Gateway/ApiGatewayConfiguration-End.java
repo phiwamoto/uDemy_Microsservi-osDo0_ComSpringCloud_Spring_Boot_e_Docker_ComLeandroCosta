11094705d3d5f1803a91ddc95c751846b6f1eab8
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-07-08 19:10:06 -0300

    Rotas com RouteLocator e Spring Cloud Gateway

--- Secao10/04 - Rotas com RouteLocator e Spring Cloud Gateway/ApiGatewayConfiguration-End.java
+++ Secao10/04 - Rotas com RouteLocator e Spring Cloud Gateway/ApiGatewayConfiguration-End.java
@@ -1,11 +1,6 @@
 package br.com.erudio.configuration;
 
-import java.util.function.Function;
-
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,14 +12,15 @@ public class ApiGatewayConfiguration {
 	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(p -> p.path("/get")
-				.filters(f -> f
-						.addRequestHeader("Hello", "World")
-						.addRequestParameter("Hello", "World"))
-				.uri("http://httpbin.org:80"))
+							.filters(f -> f
+								.addRequestHeader("Hello", "World")
+								.addRequestParameter("Hello", "World"))
+						.uri("http://httpbin.org:80"))
 				.route(p -> p.path("/cambio-service/**")
 						.uri("lb://cambio-service"))
 				.route(p -> p.path("/book-service/**")
-						.uri("lb://book-service"))				
+						.uri("lb://book-service"))
 				.build();
 	}
+
 }
