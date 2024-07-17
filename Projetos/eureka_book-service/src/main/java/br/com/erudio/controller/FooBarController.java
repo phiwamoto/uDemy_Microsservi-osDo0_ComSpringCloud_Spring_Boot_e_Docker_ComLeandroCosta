1adfe2f35ba952ba5e9872b3160c79ba61331476
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-07-16 21:20:39 -0300

    Retry com Resilience4j

--- Projetos/eureka_book-service/src/main/java/br/com/erudio/controller/FooBarController.java
+++ Projetos/eureka_book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -1,15 +1,27 @@
 package br.com.erudio.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
+	
+	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
 	@GetMapping("foo-bar")
+	@Retry(name = "for-bar") // Vai tentar a request 3 vezes com nome "defaut", mas criamos no application.yml a properties e definimos 5 vezes
 	public String fooBar() {
-		return "Foo-Bar!!!";
+		logger.info("Request to foo-bar is received!");
+		var response = new RestTemplate().getForEntity("http://localhot:8080/foo-bar", String.class);
+		return response.getBody();
+		
+		//return "Foo-Bar!!!";
 	}
 }
