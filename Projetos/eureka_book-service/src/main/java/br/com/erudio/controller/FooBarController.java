f608ffd53aaa9dd9fc229d15fb3eb2b64d2808de
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-07-16 22:04:04 -0300

    Conhecendo as Features de Circuit Breaker do Resilience4j

--- Projetos/eureka_book-service/src/main/java/br/com/erudio/controller/FooBarController.java
+++ Projetos/eureka_book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -16,7 +17,8 @@ public class FooBarController {
 	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
 	@GetMapping("foo-bar")
-	@Retry(name = "for-bar", fallbackMethod = "fallbackMethod") // Vai tentar a request 3 vezes com nome "defaut", mas criamos no application.yml a properties e definimos 5 vezes
+	//@Retry(name = "for-bar", fallbackMethod = "fallbackMethod") // Vai tentar a request 3 vezes com nome "defaut", mas criamos no application.yml a properties e definimos 5 vezes
+	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod") 
 	public String fooBar() {
 		logger.info("Request to foo-bar is received!");
 		var response = new RestTemplate().getForEntity("http://localhot:8080/foo-bar", String.class);
