63fca0a7b903689dc04edfa98e3027c2ffdff4cf
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-09-03 23:23:24 -0300

    Ajustando as Rotas no API Gateway

--- Projetos/eureka_api-gateway/src/main/java/br/com/erudio/configuration/ApiGatewayConfiguration.java
+++ Projetos/eureka_api-gateway/src/main/java/br/com/erudio/configuration/ApiGatewayConfiguration.java
@@ -1,30 +1,34 @@
 package br.com.erudio.configuration;
 
-import java.util.function.Function;
+//import java.util.function.Function;
+//
+//import org.springframework.cloud.gateway.route.Route;
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.Buildable;
+//import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 
-import org.springframework.cloud.gateway.route.Route;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
+//@Configuration
 public class ApiGatewayConfiguration {
 	
-	@Bean
-	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p.path("/get")
-				.filters(f -> f
-						.addRequestHeader("Hello", "World")
-						.addRequestParameter("Hello", "World"))
-				.uri("http://httpbin.org:80"))
-				.route(p -> p.path("/cambio-service/**")
-						.uri("lb://cambio-service"))
-				.route(p -> p.path("/book-service/**")
-						.uri("lb://book-service"))				
-				.build();
-	}
+// Essa classe pode ser deletada, pois passamos para o application.yml as configuracoes
+// nao apagamos devido 	
+	
+	
+//	@Bean
+//	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route(p -> p.path("/get")
+//				.filters(f -> f
+//						.addRequestHeader("Hello", "World")
+//						.addRequestParameter("Hello", "World"))
+//				.uri("http://httpbin.org:80"))
+//				.route(p -> p.path("/cambio-service/**")
+//						.uri("lb://cambio-service"))
+//				.route(p -> p.path("/book-service/**")
+//						.uri("lb://book-service"))				
+//				.build();
+//	}
 }
