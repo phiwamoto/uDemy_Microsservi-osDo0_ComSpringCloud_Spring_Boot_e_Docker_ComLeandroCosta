a7fa495426def625a71bb9db61ccfdb00be0f18b
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-09-03 23:08:34 -0300

    Configurando o SpringDoc OpenAPI UI (Swagger) nos dois Microservices

--- Projetos/eureka_cambio-service/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
+++ Projetos/eureka_cambio-service/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.License;
 
 @OpenAPIDefinition(info =
-        @Info(title = "Book Service API",
+        @Info(title = "Cambio Service API",
                 version = "v1",
                 description = "Documentation of Cambio Service API"))
 public class OpenApiConfiguration {
@@ -20,7 +20,7 @@ public class OpenApiConfiguration {
 			.components(new Components())
 			.info(
 				new io.swagger.v3.oas.models.info.Info()
-				.title("Book Service API")
+				.title("Cambio Service API")
 				.version("v1")
 				.license(
 					new License()
