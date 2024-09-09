382d8d9acd6ae6a122189bb1029cea7f68eb5221
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2024-09-08 21:55:33 -0300

    Adicionar o SpringDoc OpenAPI UI ao API Gateway

--- Projetos/eureka_api-gateway/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
+++ Projetos/eureka_api-gateway/src/main/java/br/com/erudio/configuration/OpenApiConfiguration.java
@@ -3,8 +3,8 @@ package br.com.erudio.configuration;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Configuration;
 
